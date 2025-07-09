b53b84e4657503e89cdcbf6717795de3be57f78d
iHacker777 <89131612+iHacker777@users.noreply.github.com>
2025-07-09 08:09:13 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# config.py
+TELEGRAM_TOKEN = "7535748414:AAE_OvBYuCSttVSkaOHnhGkF5Xvqy7HQq7o"
+TELEGRAM_CHAT_ID = -1002792849989 #-1002686984122 #-4850115551    # CAPTCHA SOLVER PAYATOM group
+CREDENTIALS_CSV = "tmb_credentials.csv"
+TWO_CAPTCHA_API_KEY = "8684ec98a76c6fd31ce4c0cb127ee3a4"
+
+# Autobank upload endpoint
+AUTOBANK_UPLOAD_URL = "https://tecpay.in/data_store/receive.php"
